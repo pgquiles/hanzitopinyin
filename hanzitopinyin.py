1f9fb17ddb1ff28d89980bcc8f58f29f31fe678e
Pau Garcia Quiles <pgquiles@elpauer.org>
2022-10-17 22:48:17 +0200

    Add DeepL translation capabilities (WIP)

--- hanzitopinyin.py
+++ hanzitopinyin.py
@@ -6,13 +6,16 @@
 import os
 import sys
 import argparse
+
+import deepl
 import pysrt
 from xpinyin import Pinyin
+import configparser
 
 
-def hanzi_to_pinyin(input, output, format, tones):
+def hanzi_to_pinyin(my_input, my_output, my_format, my_tones, my_translator):
     # Use a breakpoint in the code line below to debug your script.
-    subs = pysrt.open(input)
+    subs = pysrt.open(my_input)
     p = Pinyin()
     for sub in subs:
         my_pinyin: str
@@ -20,28 +23,30 @@ def hanzi_to_pinyin(input, output, format, tones):
 
         if sub and sub.text:
             my_text = sub.text
-            my_pinyin = p.get_pinyin(my_text, tone_marks=tones)
+            my_pinyin = p.get_pinyin(my_text, tone_marks=my_tones)
 
-        if format == 1:
+        if my_format == 1:
             sub.text = my_pinyin
-        elif format == 2:
-            sub.text = my_text + '\n' +  my_pinyin
+        elif my_format == 2:
+            sub.text = my_text + '\n' + my_pinyin
+        elif my_format == 3:
+            my_english = my_translator.translate_text(my_text, target_lang="EN")
+            sub.text = my_english + '\n' + my_text + '\n' + my_pinyin
 
-    subs.save(output)
+    subs.save(my_output)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--output", nargs='?', type=str, help="Output file", action="store", default="")
-    parser.add_argument("-f", "--format", type=int, help="Destination format. 1=pinyin (default), 2=hanzi+pinyin",
+    parser.add_argument("-f", "--format", type=int, help="Destination format. 1=Pinyin (default), 2=Hanzi+Pinyin, 3=English+Hanzi+Pinyin",
                         action="store", default=1)
     parser.add_argument("-t", "--tones", type=int, help="Tones: 1=marks (default), 2=numbers", action="store", default=1)
     parser.add_argument("-v", "--verbose", action="count", default=0, help="Verbose output, showing intermediate steps")
     parser.add_argument("file", type=str, help="Input SRT file")
     args = parser.parse_args()
 
-    output = ''
-#    print("ARGS.OUTPUT = " + args.output)
+    output = ""
     if args.output:
         output = args.output
     else:
@@ -49,6 +54,11 @@ if __name__ == '__main__':
             output = os.path.splitext(args.file)[0].replace('.zh_CN-hanzi', '') + '.zh_CN-pinyin.srt'
         elif args.format == 2:
             output = os.path.splitext(args.file)[0].replace('.zh_CN-hanzi', '') + '.zh_CN-hanzi+pinyin.srt'
+        elif args.format == 3:
+            config = configparser.ConfigParser()
+            config.read("hanzitopinyin.conf")
+            auth_key = config['deepl']['token']
+            translator = deepl.Translator(auth_key)
 
     tones = ''
     if args.tones:
@@ -62,4 +72,4 @@ if __name__ == '__main__':
     else:
         tones = 'marks'
 
-    hanzi_to_pinyin(args.file, output, args.format, tones)
+    hanzi_to_pinyin(args.file, output, args.format, tones, translator)
